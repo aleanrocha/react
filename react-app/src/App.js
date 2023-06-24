87093ca7d0aaff8683f753dbc986c0dc8dae9456
aleanrocha <ale.oficial.me@gmail.com>
2023-06-24 16:32:30 -0300

    meu primeiro app em react

---
+++ react-app/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Hello World</h1>
+     <p>Curso de React - Alterando Documento</p>
+    </div>
+  );
+}
+
+export default App;
