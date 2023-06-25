5cf9879324a617c4f693938e89120ed812652bf9
aleanrocha <ale.oficial.me@gmail.com>
2023-06-25 11:53:36 -0300

    entendendo o jsx

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,10 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'React'
+  const name2 = 'JSX'
+
+  function soma(a, b) {
+    return a + b
+  }
+
+  const url = 'https://via.placeholder.com/150'
+
   return (
     <div className="App">
-     <h1>Hello World</h1>
-     <p>Curso de React - Alterando Documento</p>
+     <h1>Curso de {name}</h1>
+     <p>Alterando o {name2}</p>
+     <p>A soma de 1 + 1 = {soma(1, 1)}</p>
+     <img src={url} alt="imagem aleatória"></img>
     </div>
   );
 }
