3b31c18d735ea2dc2d011e799e25867b98c2b4bf
aleanrocha <ale.oficial.me@gmail.com>
2023-06-29 18:41:52 -0300

    Utilizando o useState

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,14 +1,12 @@
 import './App.css'
-import Evento from './components/Evento'
 import Form from './components/Form'
 
 function App() {
   return (
     <div className="App">
       <h1>Curso de react</h1>
-      <h2>Trabalhando com Eventos em React.</h2>
-      <Evento />
-      {/* <Form /> */}
+      <h2>Utilizando o (useState)</h2>
+      <Form />
     </div>
   )
 }
--- react-app/src/components/Form.js
+++ react-app/src/components/Form.js
@@ -1,14 +1,45 @@
+import { useState } from 'react'
+import styles from './Form.module.css'
+
 function Form() {
+
   const submit = (e) => {
     e.preventDefault()
-    console.log("enviado com sucesso!")
+    console.log(`Usuário ${name}foi cadastrado com a senha ${password}`)
   }
+
+  const [name, setName] = useState()
+  const [password, SetPassword] = useState()
+
   return (
-    <form onSubmit={submit}>
-      <div><input type="text" placeholder="Digite seu nome"/></div>
-      <div><input type="text" placeholder="Digite seu sobrenome"/></div>
-      <div><input type="submit" value="Enviar" /></div>
-    </form>
+    <main>
+      <h2>Cadastro</h2>
+      <form onSubmit={submit}>
+        <div>
+          <label htmlFor="user">Nome:</label>
+          <input 
+          type="text" 
+          id="user" 
+          name="user" 
+          placeholder="Digite seu nome"
+          onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Senha:</label>
+          <input t
+          type="password" 
+          id="password" 
+          name="password" 
+          placeholder="*********"
+          onChange={(e) => SetPassword(e.target.value)}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Entrar" />
+        </div>
+      </form>
+    </main>
   )
 }
 export default Form
