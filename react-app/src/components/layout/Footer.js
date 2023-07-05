18189a129d7298bb261c6911b22ba5aa34bb9525
aleanrocha <ale.oficial.me@gmail.com>
2023-07-05 11:51:35 -0300

    react icons

--- react-app/src/components/layout/Footer.js
+++ react-app/src/components/layout/Footer.js
@@ -1,5 +1,13 @@
 import styles from './Footer.module.css'
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa6'
+
 function Footer() {
-  return <footer className={styles.footer}>Todos os direitos reservados</footer>
+  return (
+  <footer className={styles.footer}>
+    Todos os direitos reservados
+    <FaFacebook />
+    <FaInstagram />
+    <FaLinkedin />
+  </footer>)
 }
 export default Footer
