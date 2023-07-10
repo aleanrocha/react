f6b691f2537f0a4d45860eafcd1de599de2c5ba2
aleanrocha <ale.oficial.me@gmail.com>
2023-07-09 22:35:29 -0300

    criando a home

--- costs/src/components/pages/Home.jsx
+++ costs/src/components/pages/Home.jsx
@@ -1,6 +1,14 @@
+import styles from './Home.module.css'
+import savings from '../../assets/savings.svg'
+import LinkButton from '../layout/LinkButton'
 const Home = () => {
   return (
-    <h1>Home</h1>
+    <section className={styles.home_container}>
+      <h1>Bem vindo ao <span>Costs</span></h1>
+      <p>Não perca tempo, Começe a gerenciar seus projetos agora mesmo!</p>
+      <LinkButton to='/NewProject' text='Criar projeto' />
+      <img src={savings} alt="costs" />
+    </section>
   )
 }
 export default Home
