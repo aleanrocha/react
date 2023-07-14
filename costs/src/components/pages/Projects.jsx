ce788ceb076aeffed4ff4e05e55e137efb99e5f6
aleanrocha <ale.oficial.me@gmail.com>
2023-07-13 23:22:22 -0300

    criando dashboard

--- costs/src/components/pages/Projects.jsx
+++ costs/src/components/pages/Projects.jsx
@@ -1,14 +1,23 @@
-import Message from '../layout/Message'
+import styles from './Projects.module.css'
 import { useLocation } from 'react-router-dom'
+import Message from '../layout/Message'
+import Container from '../layout/Container'
+import LinkButton from '../layout/LinkButton'
 
 const Projects = () => {
   const location = useLocation()
   let message = ''
   if (location.state) {message = location.state.message}
   return (
-    <div>
-      <h1>Meus Projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButton to='/NewProject' text='Criar projeto' />
+      </div>
       {message && (<Message msg={message} type='success'/>)}
+      <Container customClass='start'>
+        <p>Projetos...</p>
+      </Container>
     </div>
   )
 }
