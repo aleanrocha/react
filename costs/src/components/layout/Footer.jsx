6a9a4296ddc1b6664a09ad63355c0e5b0b903ff4
aleanrocha <ale.oficial.me@gmail.com>
2023-07-15 13:29:32 -0300

    melhorando o footer

--- costs/src/components/layout/Footer.jsx
+++ costs/src/components/layout/Footer.jsx
@@ -4,12 +4,12 @@ import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p><span className={styles.emphasis} >Costs</span> - Copyright &copy; 2023</p>
       <ul className={styles.social_list}>
         <li><FaFacebook /></li>
         <li><FaInstagram /></li>
         <li><FaLinkedin /></li>
       </ul>
+      <p><span className={styles.emphasis} >Costs</span> - Copyright &copy; 2023</p>
     </footer>
   )
 }
