10e85882a0b8f82a839ef12a392e99747ddc80be
aleanrocha <ale.oficial.me@gmail.com>
2023-07-17 11:45:11 -0300

    criandoa pagina de contato

--- costs/src/components/pages/Contact.jsx
+++ costs/src/components/pages/Contact.jsx
@@ -1,6 +1,35 @@
+import styles from './Contact.module.css'
+import Container from '../layout/Container'
+import Input from '../form/Input'
+import SubmitButton from '../form/SubmitButton'
 const Contact = () => {
   return (
-    <h1>Contato</h1>
+    <div className={styles.contact_container}>
+      <h1>Contato</h1>
+      <form>
+        <Input
+          type='text'
+          name='name'
+          placeholder='Digite seu nome'
+        />
+        <Input
+          type='text'
+          name='lastname'
+          placeholder='Digite seu sobrenome'
+        />
+        <Input
+          type='number'
+          name='phone'
+          placeholder='Digite seu telefone'
+        />
+        <Input
+          type='email'
+          name='email'
+          placeholder='Digite seu e-mail'
+        />
+        <SubmitButton text='Enviar' />
+      </form>
+    </div>
   )
 }
 export default Contact
